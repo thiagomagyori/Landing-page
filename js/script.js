0d937e6a98a82e39f0dbfb888ddf72db356d0e29
Thiago Magyori <thiagosilvamagyori@mail.com>
2022-11-08 23:21:26 -0300

    Foi adicionado forma de paamento mercado

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+   function funcao1()
+   {
+   var obj = document.getElementById('youtube').scrollIntoView();
+   }
+/*função que jogo o usuario ao clicar no baão para o video para que ele assita o video*/
