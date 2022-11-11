b5c61e08534810c9119bd4e6493e5e083401bbde
Thiago Magyori <thiagosilvamagyori@mail.com>
2022-11-11 00:14:24 -0300

    Foi feito todos ajuste nos cards

--- js/script.js
+++ js/script.js
@@ -4,3 +4,10 @@
    var obj = document.getElementById('youtube').scrollIntoView();
    }
 /*função que jogo o usuario ao clicar no baão para o video para que ele assita o video*/
+
+
+/*Função para orientar o usuario antes de compra*/
+function funcao2()
+{
+   alert("AO CLICAR EM OK VOCÊ ESTÁ DE ACORDO COM O CURSO NÃO FAZEMOS A DEVOLUÇÃO DE DINHEIRO!")
+}
